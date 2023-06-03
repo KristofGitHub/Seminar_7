a044c6fef0c90eb7037be1d4c2fef4f16fd61ff1
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-03 20:50:32 +0500

    1st commit: methods for creating and printing 2D arrays

--- 01 - classwork/Program.cs
+++ 01 - classwork/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] myArray = Create2DMatrix(3, 3, 0, 10);
+Print2DArray(myArray, 3, 3);
+
+int[,] Create2DMatrix (int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(int[,] array, int rows, int columns)
+{
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}

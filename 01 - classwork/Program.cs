c7118beaa8348a125a1f2d224ac16ab5b242dacf
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-03 20:55:57 +0500

    2nd commit: best version

--- 01 - classwork/Program.cs
+++ 01 - classwork/Program.cs
@@ -1,5 +1,5 @@
 ﻿int[,] myArray = Create2DMatrix(3, 3, 0, 10);
-Print2DArray(myArray, 3, 3);
+Print2DArray(myArray);
 
 int[,] Create2DMatrix (int rows, int columns, int minValue, int maxValue)
 {
@@ -14,11 +14,11 @@ int[,] Create2DMatrix (int rows, int columns, int minValue, int maxValue)
     return array;
 }
 
-void Print2DArray(int[,] array, int rows, int columns)
+void Print2DArray(int[,] array)
 {
-    for(int i = 0; i < rows; i++)
+    for(int i = 0; i < array.GetLength(0); i++)
     {
-        for(int j = 0; j < columns; j++)
+        for(int j = 0; j < array.GetLength(1); j++)
         {
             Console.Write(array[i, j] + " ");
         }

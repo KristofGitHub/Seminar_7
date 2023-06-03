a5037275cd4b96eebf6befa7dcb4a430b9312b99
Vasiliev Sergei <sergei.binp@gmail.com>
2023-06-03 21:07:11 +0500

    3rd commit: modified version - full

--- 01 - classwork/Program.cs
+++ 01 - classwork/Program.cs
@@ -1,4 +1,16 @@
-﻿int[,] myArray = Create2DMatrix(3, 3, 0, 10);
+﻿Console.WriteLine("            *   *   *");
+Console.WriteLine("CREATING AND PRINTING 2D-ARRAYS CODE");
+Console.WriteLine("");
+Console.WriteLine("Input number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columnss: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of minValue: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of maxValue: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = Create2DMatrix(rows, columns, minValue, maxValue);
 Print2DArray(myArray);
 
 int[,] Create2DMatrix (int rows, int columns, int minValue, int maxValue)
@@ -16,6 +28,7 @@ int[,] Create2DMatrix (int rows, int columns, int minValue, int maxValue)
 
 void Print2DArray(int[,] array)
 {
+    Console.WriteLine("Yours array: ");
     for(int i = 0; i < array.GetLength(0); i++)
     {
         for(int j = 0; j < array.GetLength(1); j++)
